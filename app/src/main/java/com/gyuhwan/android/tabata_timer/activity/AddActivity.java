2ca9aa3cfc2dbb7add4e5b66ebab711799a757b5
gyuhwan <gmarble94@gmail.com>
2018-06-10 23:16:54 +0900

    resource ttf, icon add

---
+++ app/src/main/java/com/gyuhwan/android/tabata_timer/activity/AddActivity.java
@@ -0,0 +1,15 @@
+package com.gyuhwan.android.tabata_timer.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gyuhwan.android.tabata_timer.R;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
